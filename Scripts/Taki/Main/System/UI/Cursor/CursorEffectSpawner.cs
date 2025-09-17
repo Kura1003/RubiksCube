461d62dbb162171aa991585d126ab89e1dd1c0ab
Kura1003 <gertrud.taki@gmail.com>
2025-09-18 02:45:13 +0900

    Update CursorEffectSpawner.cs

--- Scripts/Taki/Main/System/UI/Cursor/CursorEffectSpawner.cs
+++ Scripts/Taki/Main/System/UI/Cursor/CursorEffectSpawner.cs
@@ -2,7 +2,7 @@
 using System.Threading;
 using UnityEngine;
 
-namespace Taki.Utility.UI
+namespace Taki.Main.System
 {
     [RequireComponent(typeof(Canvas), typeof(RectTransform))]
     public class CursorEffectSpawner : MonoBehaviour
